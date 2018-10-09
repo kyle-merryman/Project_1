49f3311940a37577568fa30578a1abd6c754c282
navalta3030 <navalta3030@gmail.com>
2018-10-08 18:40:03 -0700

    update

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -10,33 +10,79 @@ var config = {
 firebase.initializeApp(config);
 var database = firebase.database()
 
-var Events = [];
+var userIsLoggedIn = false;
 
+// ----------------------------- CALENDER START------------------------------------------------
 $('#modal-add-event').click(function(event){
-    event.preventDefault();
-
-    var firebaseTitle = $('#modal-name').val();
-    var firebaseEventStart = $('#modal-start').val();
-    var firebaseEventEnd = $('#modal-end').val();
-    var firebaseUrl = $('#modal-url').val();
-
-
-    var postData = {
-        title:firebaseTitle,
-        start:firebaseEventStart,
-        end:firebaseEventEnd,
-        url:firebaseUrl
-    };
-
-    database.ref('/calendarEvents/').push(postData);
-
+    // if (userIsLoggedIn){
+        event.preventDefault();
     
-
+        // obtain value from add event modal
+        var firebaseTitle = $('#modal-name').val();
+        var firebaseEventStart = $('#modal-start').val();
+        var firebaseEventEnd = $('#modal-end').val();
+        var firebaseUrl = $('#modal-url').val();
+        var firebaseAddress = $('#modal-address').val();
+    
+        var postData = {
+            title:firebaseTitle,
+            start:firebaseEventStart,
+            end:firebaseEventEnd,
+            url:firebaseUrl,
+            address: firebaseAddress,
+        };
+        database.ref('/calendarEvents/').push(postData);
+    // } else {
+    //     $('.close').trigger('click');
+    //     $('#Signup-button-modal').trigger('click');
+    // }
 });
 
+// obtain data from Firebase and plot data on fullcalendar
 database.ref('/calendarEvents/').on("child_added", function(snapshot) {
     $('#calendar').fullCalendar('renderEvent', snapshot.val());
-
 }, function(errorObject) {
     console.log("The read failed: " + errorObject.code);
 });
+
+// ----------------------------- CALENDER END------------------------------------------------
+
+
+// ----------------------------- USER ACCOUNT START------------------------------------------------
+
+// SIGNUP
+$('#modal-signup').click(function(event){
+    event.preventDefault();
+
+    // obtain value from add event modal
+    var firebaseemail = $('#modal-signupemail').val();
+    var firebaseusername = $('#modal-signupuser').val();
+    var firebasepassword = $('#modal-signuppassword').val();
+
+    var postData = {
+        email: firebaseemail,
+        username: firebaseusername,
+        password: firebasepassword
+    };
+    database.ref('/accounts/').push(postData);
+});
+
+// LOGIN
+$('#modal-login').click(function(event){
+    var loginUsername = $('#modal-loginuser').val();
+    var loginPassword = $('#modal-loginpassword').val()
+
+    database.ref('/accounts/').on("child_added", function(snapshot) {
+        var fireBaseAccount = snapshot.val();
+        if (fireBaseAccount.username === loginUsername && fireBaseAccount.password === loginPassword){
+            userIsLoggedIn = true;
+        } else {
+            console.log('stop it');
+        }
+    }, function(errorObject) {
+        console.log("The read failed: " + errorObject.code);
+    });
+});
+
+
+// ----------------------------- USER ACCOUNT END------------------------------------------------
