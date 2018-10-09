ad1d2e349fdcc75538a77c27ef50fb29552ee22f
thirteen <41817587+maguirre13@users.noreply.github.com>
2018-10-08 19:53:05 -0700

    add favicon and update map default location

--- assets/javascript/mapbox.js
+++ assets/javascript/mapbox.js
@@ -1,25 +1,31 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoiZ29uemFsb24zMTQwIiwiYSI6ImNqbW53b2RsMDBmN3YzcHFvemVqMjcwMWgifQ.uSJyJkrDOWjV8xfUcaREtA';
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiZ29uemFsb24zMTQwIiwiYSI6ImNqbW53b2RsMDBmN3YzcHFvemVqMjcwMWgifQ.uSJyJkrDOWjV8xfUcaREtA";
 
 var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v9',
-    center: [-87.6321, 41.8362],
-    boxZoom: true,
-    showZoom: true,
-    fadeDuration: 2000,
-    trackResize: true,
-    zoom: 13,
+  container: "map",
+  style: "mapbox://styles/mapbox/streets-v9",
+  center: [-121.4960761, 38.5810365],
+  boxZoom: true,
+  showZoom: true,
+  fadeDuration: 2000,
+  trackResize: true,
+  zoom: 13
 });
 // Add geolocate control to the map.
-map.addControl(new mapboxgl.GeolocateControl({
+map.addControl(
+  new mapboxgl.GeolocateControl({
     positionOptions: {
-        enableHighAccuracy: true
+      enableHighAccuracy: true
     },
     trackUserLocation: true
-}));
+  })
+);
 
 map.addControl(new mapboxgl.FullscreenControl());
 
-map.addControl(new MapboxDirections({
+map.addControl(
+  new MapboxDirections({
     accessToken: mapboxgl.accessToken
-}), 'top-left');
+  }),
+  "top-left"
+);
