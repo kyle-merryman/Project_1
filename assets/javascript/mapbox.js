51e6d9659aa0b91a7f7c86c54849e62992116368
navalta3030 <navalta3030@gmail.com>
2018-10-09 18:52:20 -0700

    work

--- assets/javascript/mapbox.js
+++ assets/javascript/mapbox.js
@@ -6,20 +6,38 @@ var map = new mapboxgl.Map({
     center: [-87.6321, 41.8362],
     boxZoom: true,
     showZoom: true,
-    fadeDuration: 2000,
+    fadeDuration: 500,
     trackResize: true,
     zoom: 13,
+    
 });
 // Add geolocate control to the map.
-map.addControl(new mapboxgl.GeolocateControl({
-    positionOptions: {
-        enableHighAccuracy: true
-    },
-    trackUserLocation: true
+// map.addControl(new MapboxGeocoder({
+//     accessToken: mapboxgl.accessToken
+// }));
+
+
+
+function forwardGeocoder(query) {
+    var matchingFeatures = [];
+    for (var i = 0; i < customData.features.length; i++) {
+        var feature = customData.features[i];
+        // handle queries with different capitalization than the source data by calling toLowerCase()
+        if (feature.properties.title.toLowerCase().search(query.toLowerCase()) !== -1) {
+            // add a tree emoji as a prefix for custom data results
+            // using carmen geojson format: https://github.com/mapbox/carmen/blob/master/carmen-geojson.md
+            feature['place_name'] = '🌲 ' + feature.properties.title;
+            feature['center'] = feature.geometry.coordinates;
+            feature['place_type'] = ['park'];
+            matchingFeatures.push(feature);
+        }
+    }
+    return matchingFeatures;
+}
+
+map.addControl(new MapboxGeocoder({
+    accessToken: mapboxgl.accessToken,
+    localGeocoder: forwardGeocoder,
+    zoom: 14,
+    placeholder: "Enter search e.g. Lincoln Park"
 }));
-
-map.addControl(new mapboxgl.FullscreenControl());
-
-map.addControl(new MapboxDirections({
-    accessToken: mapboxgl.accessToken
-}), 'top-left');
